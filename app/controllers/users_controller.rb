5abe05b60dd73ddefde184ab97fa1d69a79c8b70
Michael Adamski <mvadamski@gmail.com>
2017-07-08 20:36:53 -0400

    user profile page, link to complete profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,20 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @photos = Photo.where(user_id: @user.id)
+    @current_user = current_user
+    @complete_profile = false
+    if @current_user.first_name.nil? || @current_user.last_name.nil? ||
+      @current_user.mother.nil? || @current_user.father.nil? || @current_user.city.nil? ||
+      @current_user.state.nil? || @current_user.phone_number.nil? ||
+      @current_user.address.nil?
+      @complete_profile = false
+    else
+      @complete_profile = true
+    end
+  end
+
+  def edit
+    @user = current_user
   end
 
   def destroy
