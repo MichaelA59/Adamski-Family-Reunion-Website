0be57004b1b100553761614fdeb8854f2dbb4945
Michael Adamski <mvadamski@gmail.com>
2017-07-09 22:45:05 -0400

    added user edit page with more fields and constants for dropdowns

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,18 @@ class UsersController < ApplicationController
 
   def edit
     @user = current_user
+    @mother = User::MOTHER
+    @father = User::FATHER
+    @state = User::STATE
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -28,5 +40,23 @@ class UsersController < ApplicationController
     current_user.destroy
 
     redirect_to root
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :first_name,
+      :last_name,
+      :name,
+      :email,
+      :image,
+      :mother,
+      :father,
+      :city,
+      :state,
+      :phone_number,
+      :address
+    )
   end
 end
