7074a6fb8844db20ca4f578067ce730586f02d9d
Michael Adamski <mvadamski@gmail.com>
2017-07-25 16:20:07 -0400

    photos uploader stuff

--- app/uploaders/photos_uploader.rb
+++ app/uploaders/photos_uploader.rb
@@ -14,17 +14,18 @@ class PhotosUploader < CarrierWave::Uploader::Base
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
+
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
+  def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+    ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
   #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+    "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  end
 
   def auto_orient
     manipulate! do |img|
