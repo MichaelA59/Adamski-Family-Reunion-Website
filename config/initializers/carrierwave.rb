881f4e165481852e1aacfb378f19a52694243255
Michael Adamski <mvadamski@gmail.com>
2017-07-25 16:54:57 -0400

    thing

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,7 +6,7 @@ CarrierWave.configure do |config|
       provider: "AWS",
       aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
       aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-      region: 'us-east-1'
+      region: 'us-east-2'
 
     }
     config.fog_directory  = ENV["S3_BUCKET"]
