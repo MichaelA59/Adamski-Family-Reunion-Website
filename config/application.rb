0a69281e0d98d25ef209184c0e1c70c81f781d05
Michael Adamski <mvadamski@gmail.com>
2017-07-25 14:05:40 -0400

    react precompiler nonsense

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,6 @@ module AdamskiFamilyReunionWebsite
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.precompile += [Rails.root.join("react", "**", "*.js")]
   end
 end
-
-config.assets.precompile += [Rails.root.join("react", "**", "*.js")]
