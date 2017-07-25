50936ee485040f3585ce04cf13d5b9fd6ed48fb4
Michael Adamski <mvadamski@gmail.com>
2017-07-25 14:42:46 -0400

    removed APP component from react side

--- react/src/main.js
+++ react/src/main.js
@@ -2,18 +2,7 @@ import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom'
 import ReactDOM from 'react-dom';
-// import App from './App';
 import UsersContainer from './containers/UsersContainer';
-
-
-// $(function() {
-//   if(document.getElementById('app')) {
-//     ReactDOM.render(
-//       <App />,
-//       document.getElementById('app')
-//     );
-//   }
-// });
 
 $(function() {
   if(document.getElementById('user-index')) {
