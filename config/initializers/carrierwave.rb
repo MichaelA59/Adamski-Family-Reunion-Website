0d1fd3058db4ce7f24bfe5bd394a3e829f78719a
Michael Adamski <mvadamski@gmail.com>
2017-07-25 21:28:19 -0400

    please jesus

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,11 @@
 require 'carrierwave/orm/activerecord'
 
 CarrierWave.configure do |config|
-  if !Rails.env.test?
-    config.fog_credentials = {
-      provider: "AWS",
-      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-      :region => 'us-east-2'
-    }
-    config.fog_directory  = ENV["S3_BUCKET"]
-  end
+  config.fog_credentials = {
+    provider: "AWS",
+    aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+    :region => 'us-east-2'
+  }
+  config.fog_directory  = ENV["S3_BUCKET"]
 end
